87a83be39a5aa43fccae1409147494311651a841
karrynest <43960622+karrynest@users.noreply.github.com>
2019-09-30 16:40:07 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+__VERSION__ = '0.0.2'
+
+setup(name='adamod',
+      version=__VERSION__,
+      description='AdaMod optimization algorithm, build on PyTorch.',
+      long_description=open("README.md", errors='ignore').read(),
+      long_description_content_type="text/markdown",
+      keywords=['machine learning', 'deep learning'],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      ],
+      url='https://github.com/karrynest/AdaMod',
+      author='Jianbang Ding',
+      author_email='jianbangding@pku.edu.cn',
+      license='Apache',
+      packages=['adamod'],
+      install_requires=[
+          'torch>=0.4.0',
+      ],
+      zip_safe=False,
+      python_requires='>=3.6.0')
